8bbfb0ea13768fad0946b2203f44585d6b520b7a
Suyashkala18 <suyash1.kala@lntinfotech.com>
2021-01-03 15:01:52 +0530

    suyash kala first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AdminloginComponent } from './adminlogin/adminlogin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AdminloginComponent
   ],
   imports: [
     BrowserModule,
