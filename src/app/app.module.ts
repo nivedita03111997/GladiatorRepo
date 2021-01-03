df479512e6a7db56d81ad7d301d85cb4a9d093d4
Nivedita Bauskar <nivedita.bauskar@lntinfotech.com>
2021-01-03 15:50:24 +0530

    commit1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
+
 import { DashboardComponent } from './dashboard/dashboard.component';
-=======
-<<<<<<< HEAD
+
 import { LoginComponent } from './login/login.component';
 import { AdminloginComponent } from './adminlogin/adminlogin.component';
-=======
+
 import { UserDetailsForAdminComponent } from './user-details-for-admin/user-details-for-admin.component';
 import { ActivateUserComponent } from './activate-user/activate-user.component';
->>>>>>> 15c584050e60099ac1e9cdc88d0fe3f977a28519
->>>>>>> a6d07a86ab10dc2a05787dcc07b4a63c6a146c9e
+
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    DashboardComponent
-=======
-<<<<<<< HEAD
+    DashboardComponent,
     LoginComponent,
-    AdminloginComponent
-=======
+    AdminloginComponent,
     UserDetailsForAdminComponent,
     ActivateUserComponent
->>>>>>> 15c584050e60099ac1e9cdc88d0fe3f977a28519
->>>>>>> a6d07a86ab10dc2a05787dcc07b4a63c6a146c9e
   ],
   imports: [
     BrowserModule,
